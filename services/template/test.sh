8efda71225e19d48a11d0591e79d612d3359f582
Trevor Phippard <trevorphippard@ee:e4:e9:2a:b7:26.home>
2025-08-23 17:06:01 -0400

    dev

--- services/template/test.sh
+++ services/template/test.sh
@@ -6,7 +6,7 @@ BASE_URL="http://localhost:8000/api/template"
 echo "===> Testing valid POST"
 curl -s -X POST "$BASE_URL/test" \
   -H "Content-Type: application/json" \
-  -d '{"testString": "hello world"}' | jq
+  -d '{"testString": "hello world!"}' | jq
 echo -e "\n"
 
 echo "===> Testing missing parameter"
